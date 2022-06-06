9abcf1845049512d8d8f3b8dc6a0bf9a3fc23d42
carlosjtandem <soycarlosjavier14@gmail.com>
2022-06-06 17:05:20 -0500

    Added Queue Collection

---
+++ Colecciones_Queue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+Queue myQ= new Queue();  //FIFO
+
+//adicionamos objetos
+myQ.Enqueue("Manzana");
+myQ.Enqueue("Fresa");
+myQ.Enqueue("Pera");
+myQ.Enqueue("Limon");
+
+
+foreach (var fruta in myQ)
+{
+    Console.WriteLine("-- {0}", fruta);  // imprime en el orden que se ingresa.
+}
+
+// Obtener un objeto  DEQUEUE
+
+Console.WriteLine("Dequeue {0}",myQ.Dequeue());
+//Console.WriteLine("Dequeue {0}", myQ.Dequeue());
+
+foreach (string fruta in myQ)
+{
+    Console.WriteLine("Despues del queue--- {0}",fruta);
+}
+
+
+//Adicionamis objetos
+myQ.Enqueue("coco");
+myQ.Enqueue("babaco");
+myQ.Enqueue("ciruela");
+
+//observamo sobjecto con peek
+Console.WriteLine("Peek {0}", myQ.Peek());
+
+// Conteo de objetos
+Console.WriteLine(myQ.Count);
+
+//verificamos si existe elemento
+Console.WriteLine(myQ.Contains("coco"));
+Console.WriteLine(myQ.Contains("coco Loco"));
+
