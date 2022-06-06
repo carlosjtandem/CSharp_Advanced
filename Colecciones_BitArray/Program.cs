6768110d9805655078a84c56f53de46069e0f84f
carlosjtandem <soycarlosjavier14@gmail.com>
2022-06-06 12:24:27 -0500

    features with BitArray

--- Colecciones_BitArray/Program.cs
+++ Colecciones_BitArray/Program.cs
@@ -15,11 +15,40 @@ Console.WriteLine(miArreglo.Get(8));
 miArreglo.Set(3, true);
 MuestraArreglo(miArreglo);
 
+//Clonamos un bit Array 
+BitArray arreglo2 = (BitArray)miArreglo.Clone();  // Importante el cast a BitArray
+MuestraArreglo(arreglo2, "array clonado\n");
+
+//Invertimos un bitArray .. NOT
+arreglo2.Not();
+MuestraArreglo(arreglo2, "invert");
+
+//creamos otros array
+BitArray arreglo3 = new BitArray(new byte[] { 5, 7, 9, 13, 15 });
+MuestraArreglo(arreglo3, "Arreglo3");
+Console.WriteLine();
+
+//and entre arreglos
+arreglo3.Or(miArreglo);
+//muestra
+MuestraArreglo(miArreglo, "miArreglo");
+MuestraArreglo(arreglo3, "miArreglo3");
+
+
+//miArreglo.And(miArreglo);
+//MuestraArreglo(miArreglo, "miArreglo");
+
+
+
+//XOR entre arreglos
+//devuelve 1 si son diferentes 1 xor 0 = 1
+//devuelve 0 si son iguales 0 xor 0 = 0
+
 
 static void MuestraArreglo(BitArray pArreglo, string pNombre = "")
 {
     int c = 0;
-    Console.Write("{0}\t" + pNombre);
+    Console.Write("{0}\t", pNombre);
     foreach (bool item in pArreglo)
     {
         c++;
