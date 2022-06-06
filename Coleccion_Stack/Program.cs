a6cbf39514879365767685787e3204ccb9a14d14
carlosjtandem <soycarlosjavier14@gmail.com>
2022-06-06 18:03:07 -0500

    Stack - LIFO

---
+++ Coleccion_Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+//Stack es una coleccion tipo LIFO
+
+Stack miStack= new Stack();
+miStack.Push("Manzana");
+miStack.Push("banano");
+miStack.Push("pera");
+miStack.Push("mandarina");
+
+
+//iteramos el stack
+foreach (string fruta in miStack)
+{
+    Console.WriteLine("{0}",fruta);
+}
+
+//pop.. obtener un elemento del stack -- lo saca del stack si imprimos nuevamente entonces ya no esta ese elemento
+Console.WriteLine("El primer elemento del stack es:"+miStack.Pop());
+foreach (string fruta in miStack)
+{
+    Console.WriteLine("{0}", fruta);
+}
+
+//peek.. obtener un elemento del stack -- NO lo saca del stack SOLO lo muestra
+Console.WriteLine("El primer elemento del stack es:" + miStack.Peek());
+foreach (string fruta in miStack)
+{
+    Console.WriteLine("{0}", fruta);
+}
