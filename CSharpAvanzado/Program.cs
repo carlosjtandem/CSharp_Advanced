3207880c6fefc76eafc956ff7cf2d7c95c3a7a35
carlosjtandem <soycarlosjavier14@gmail.com>
2022-06-06 11:55:31 -0500

    Collections

---
+++ CSharpAvanzado/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Type tipo;
+//1 ARRAY LIST
+ArrayList palabras = new ArrayList();
+palabras.AddRange(new string[] { "Hola", "Carlos", "como vas?" });
+Console.WriteLine("Cantidad elementos:{0}", palabras.Count);
+//REcorremos el ciclo con for
+for (int i = 0; i < palabras.Count; i++)
+{
+    Console.WriteLine("{0}", palabras[i]);
+}
+Console.WriteLine("");
+Console.WriteLine("----------------------------");
+
+//Add element- with Add()
+palabras.Add("Bien y tu?");
+palabras.Add(3);
+//REcorremos el ciclo con foreach
+foreach (var palabra in palabras)
+{
+    Console.WriteLine("{0}", palabra + " tipo:" + palabra.GetType());
+
+}
+
+Console.WriteLine("");
+Console.WriteLine("----------------------------");
+//Array de enteros
+ArrayList valores = new ArrayList();
+valores.Add(10);
+valores.Add(20);
+valores.Add(40);
+
+
+//Añadir en un indice particular
+valores.Insert(2, 30);
+
+int r = 0;
+for (int i = 0; i < valores.Count; i++)
+{
+    r = (int)valores[i];
+    Console.WriteLine(valores[i] + "--" + i.GetType());
+}
+
+//remover valores del array por valor o indice
+valores.Remove(10);  // valores.RemoveAt(0)  // importante validar si el indice existe
+for (int i = 0; i < valores.Count; i++)
+{
+    r = (int)valores[i];
+    Console.WriteLine(valores[i] + "--" + i.GetType());
+}
+
+
+
+
+
+
+//What happened if I add a int value?
---
+++ Colecciones_BitArray/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+BitArray miArreglo = new BitArray(new byte[] { 1, 2, 4, 8, 16 });
+Console.WriteLine(miArreglo.Count);
+MuestraArreglo(miArreglo);
+
+Console.WriteLine("---**--");
+
+//Obtenemos un bit en particular
+Console.WriteLine(miArreglo.Get(8));
+
+// Agregar un bit en particular por el indice
+miArreglo.Set(3, true);
+MuestraArreglo(miArreglo);
+
+
+static void MuestraArreglo(BitArray pArreglo, string pNombre = "")
+{
+    int c = 0;
+    Console.Write("{0}\t" + pNombre);
+    foreach (bool item in pArreglo)
+    {
+        c++;
+        if (item)
+            Console.Write("1");
+        else
+            Console.Write("0");
+        if (c % 8 == 0)
+            Console.Write(",");
+
+    }
+
+    Console.WriteLine();
+}
