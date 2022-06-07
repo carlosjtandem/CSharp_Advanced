8ee2c73ccdb61e279af7cba81520bee079e94f27
carlosjtandem <soycarlosjavier14@gmail.com>
2022-06-07 16:41:53 -0500

    Dynamic

---
+++ Dynamic/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Dynamic se puede ver como una forma de System.Object
+//no es fuertemente tipado
+//cualquier valor se puede asignar a dinamic
+//sim primero le asigno un string, despues le puedo cambiar a int, y despues a otro
+
+dynamic a = 5;
+Console.WriteLine("valor {0} es {1}",a, a.GetType());
+
+a = "Hola";
+Console.WriteLine("Ahora valor {0} es {1}", a, a.GetType());
+
+// no puede ser utilizado en expresiones lambda
+//dynamic se puede usar como tipo de retorno
