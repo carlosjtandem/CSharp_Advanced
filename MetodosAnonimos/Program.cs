6f1b250ac39135a7bb09b2614c9435efa6cb835a
carlosjtandem <soycarlosjavier14@gmail.com>
2022-06-09 11:41:42 -0500

    metodos anonimos

---
+++ MetodosAnonimos/Program.cs
@@ -0,0 +1,24 @@
+﻿// un metodo anonimo es un metodo que no tiene nombre
+// 
+
+using MetodosAnonimos;
+
+Punto miPunto = new Punto(3, 4);
+
+//creamos el metodo aninimo para el delegado
+//+= sirve para asignar un handler al delegado
+miPunto.mensaje += delegate ()
+{
+    Console.WriteLine("EStoy desde el metodo anonimo"); // este es el metodo anonimo que se invocara cuando invoquemos a mensaje
+};
+Console.WriteLine(miPunto.ToString());
+
+//invocamos
+miPunto.mensaje(); // esto invocara todos los handlers que tenga asignado
+
+// Tambien se puede crear mas handler al delegado , lo haremos como metodos anonimos
+miPunto.mensaje += delegate ()
+{
+    Console.WriteLine("Handler 2"); // este es el metodo anonimo que se invocara cuando invoquemos a mensaje
+};
+miPunto.mensaje(); // esto invocara todos los handlers que tenga asignado
---
+++ MetodosAnonimos/Punto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosAnonimos
+{
+    class Punto
+    {
+        public delegate void DelMensaje();
+        public DelMensaje mensaje;
+
+        private int x;
+        private int y;
+
+        public Punto(int px, int py)
+        {
+            x = px;
+            y = py;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("X={0} ,Y={1}", x, y);
+        }
+    }
+}
