c9ccd8de01bc53685c11cd09d6a0901ea47d5fcf
carlosjtandem <soycarlosjavier14@gmail.com>
2022-06-09 11:03:40 -0500

    FUNC

---
+++ DelegadoGenerico/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Handle para string
+static void HandlerString(string valor)
+{
+    Console.WriteLine("uso string como tipo y el valor es:{0}", valor);
+}
+
+static void HandlerDouble(double valor)
+{
+    Console.WriteLine("uso double como tipo y el valor es:{0}", valor);
+}
+
+//REgistro y usamos string
+miDelegado<string> deCadena = new miDelegado<string>(HandlerString);
+deCadena("Hola carlos");
+
+//Registramos y usamos doble
+miDelegado<double> deDouble = new miDelegado<double>(HandlerDouble);
+deDouble(45.7);
+
+
+public delegate void miDelegado<T>(T p);
+
---
+++ Func_y_MetodosAnonimos/Program.cs
@@ -0,0 +1,22 @@
+﻿int x = 5;
+int y = 6;
+
+// Func se usa para crear delegados en el momento 
+//Func se usa con handler que tienen un tipo de retorno
+//La diferencia con Action es que Action devuelve void
+//Func permite retornar un delegado con un tipo de valor especifico
+//    el ultimo parametro corresponde al tipo de dato retornado
+
+
+//metodo handler para Func
+static int Suma(int a, int b)
+{
+    int r = 0;
+    r = a + b;
+    return r;
+}
+
+//MAIN
+// nuestro delegado tiene 3 parametros porque son dos de la funcion suma y el ultimo es el retorno
+Func<int, int, int> delFunc = new Func<int, int, int>(Suma);
+Console.WriteLine(delFunc(x, y));
